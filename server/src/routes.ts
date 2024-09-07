a1ebf9789840406d14c070651582e6979799b3bb
Beatriz Paixão <biaperon2001@gmail.com>
2024-09-07 12:57:13 -0300

    add log errors

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,23 +5,28 @@ import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router()
 
-routes.post("/feedbacks", async (request, response) => {
+routes.post("/feedbacks", async (req, res) => {
+    const { type, comment, screenshot } = req.body;
 
-    const { type, comment, screenshot } = request.body;
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
 
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        )
 
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-    )
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot,
+        })
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot,
-    })
+        return res.status(201).send();
+    } catch (err) {
+        console.error(err);
 
-    return response.status(201).send();
+        return res.status(500).send();
+    }
 })
