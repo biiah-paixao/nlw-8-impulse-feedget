2da5fd728f52184510aca3ef2332cb35ff0853be
Beatriz Paixão <biaperon2001@gmail.com>
2024-09-07 20:32:48 -0300

    add assests file for readme

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -13,21 +13,26 @@ export const feedbackTypes = {
     image: {
       source: bugImageUrl,
       alt: 'Imagem de um inseto'
-    }
+    },
+    placeholder:
+      "Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo...",
   },
   IDEA: {
     title: 'Ideia',
     image: {
       source: ideaImageUrl,
       alt: 'Imagem de uma lampada'
-    }
+    },
+    placeholder: 
+      "Teve uma ideia de melhoria ou de nova funcionalidade? Conta pra gente!",
   },
   OTHER: {
     title: 'Outro',
     image: {
       source: thoughtImageUrl,
       alt: 'Imagem de uma nuvem de pensamento'
-    }
+    },
+    placeholder: "Queremos te ouvir. O que você gostaria de nos dizer?",
   },
 }
 
